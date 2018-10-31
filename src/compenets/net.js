55a696ad99920105a011f8a68d8884583d506856
Acidtar acidtar <muzhiu@gmail.com>
2018-10-31 14:52:08 +0800

    sparkle

--- src/compenets/net.js
+++ src/compenets/net.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
+import Onetracks from './oneTrack';
+
  class Net extends Component {
   constructor(props) {
     super(props);
   
     this.state = {
-      playList: []
+      playList: [],
+      songList: []
     }
   }
 
@@ -14,14 +17,21 @@ import axios from "axios";
     let netPlayList = await axios.get("http://127.0.0.1:3001/netList");
     
     console.log(netPlayList.data.playlist.tracks);
+    let data = netPlayList.data.playlist.tracks;
     this.setState({
-      playList: netPlayList.data.playlist.tracks
+      playList: data,
+      songList: data.map((item, index) => (<Onetracks key={index} name={item.name} id={item.id} ar={item.ar}></Onetracks>))
     });
+    console.log(this.state.songList);
   }
 
   render () {
     return (
-      <div>NET!</div>
+      <div className="netease">
+        <ul className="flex-c flex-s align-s">
+          {this.state.songList}
+        </ul>
+      </div>
     )
   }
 }
---
+++ src/compenets/oneTrack.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styleSheet/oneTrack.css";
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      trackName: props.name,
+      id: props.id,
+      ar: props.ar
+    }
+  }
+
+  render() {
+    return (
+      <li className="a-track flex">
+        <div className="track">
+          <span>{this.state.trackName}</span>
+        </div>
+        <div className="ar">
+          <span>{this.state.ar[0].name}</span>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Track;
