87270816288dff7c4c3f5f295fe54a7eba88758a
Acidtar acidtar <muzhiu@gmail.com>
2018-11-13 10:51:51 +0800

    view all tracks

--- src/compenets/oneList.js
+++ src/compenets/oneList.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import { connect } from 'react-redux';
 import Track from './oneTrack';
-// import action from '../state/playList';
 import axios from 'axios';
 
 import "../styleSheet/oneList.scss";
@@ -16,7 +14,8 @@ class List extends React.Component {
     this.state = {
       id: props.id,
       JSXTracks: [],
-      tracks: []
+      tracks: [],
+      visiableTRACKS: []
     };
   }
 
@@ -28,16 +27,17 @@ class List extends React.Component {
     let tracks = await axios.get(`http://${apiConfig.api}/listDetail?id=${this.state.id}&limit=${limit || 'all'}&offset=${offset || 0}`);
     this.setState({
       tracks: tracks.data.playlist.tracks,
-      JSXTracks: tracks.data.playlist.tracks.map((item, index) => {
-        return (<Track onClick={()=> {
-          this.props.action({
-            playList: tracks.data.playlist.tracks,
-            playIndex: index
-          });
-        }} key={index} dt={item.dt} trackName={item.name} id={item.id} ar={item.ar}></Track>)
-      }),
+      // JSXTracks: tracks.data.playlist.tracks.map((item, index) => {
+      //   return (<Track onClick={()=> {
+      //     this.props.action({
+      //       playList: tracks.data.playlist.tracks,
+      //       playIndex: index
+      //     });
+      //   }} key={item.id} dt={item.dt} trackName={item.name} id={item.id} ar={item.ar}></Track>)
+      // }),
       trackCount: tracks.data.playlist.trackCount,
-      viewAll: limit === 'all' ? true : false
+      viewAll: limit === 'all' ? true : false,
+      visiableTRACKS: limit === 'all' ? tracks.data.playlist.tracks : tracks.data.playlist.tracks.slice(0, 10)
     });
   }
 
@@ -52,14 +52,24 @@ class List extends React.Component {
             <div className="list-info">
               <h2 className="list-name">{this.props.name}</h2>
             </div>
-            <div className="play"></div>
+            {/* <div className="play"></div> */}
           </div>
           <div className="right-bottom">
             <ul>
-              {this.state.JSXTracks}
               {
-                this.state.trackCount > 10 && (<li className="a-track view-all-wap flex j-center">
-                  <div onClick={()=> {this.trackFetcher('all')}} className="view-all">
+                this.state.visiableTRACKS.map((item, index) => {
+                  return (<Track onClick={()=> {
+                    this.props.action({
+                      playList: this.state.visiableTRACKS,
+                      playIndex: index
+                    });
+                  }} key={item.id} dt={item.dt} trackName={item.name} id={item.id} ar={item.ar}></Track>)
+                })
+              }
+              {
+                this.state.trackCount > 10 && 
+                (<li onClick={this.state.viewAll ? ()=> {this.setState({visiableTRACKS: this.state.tracks.slice(0, 10), viewAll: false})} : ()=> {this.trackFetcher('all')}} className="a-track view-all-wap flex j-center">
+                  <div className="view-all">
                     {this.state.viewAll ? "Viwe less tracks" : `View ${this.state.trackCount || "loading"} tracks`}
                   </div>
                 </li>)
