bda5a97f126c34414c42e66eb387ffa2cf71881c
Acidtar acidtar <muzhiu@gmail.com>
2018-12-06 18:37:12 +0800

    [ Server Mount ] Mount

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
 export default {
-  api: 'http://207.148.102.95:3001', // modify this to your diRty-api adress,
-  search: 'http://207.148.102.95:3002'
+  api: '207.148.102.95:3001', // modify this to your diRty-api adress,
+  search: '207.148.102.95:3002'
 } 
