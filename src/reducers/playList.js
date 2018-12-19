3ec389de17173a0563461487f0e641e85834ef10
Acidtar acidtar <muzhiu@gmail.com>
2018-12-19 14:31:30 +0800

    [ Bug Fix ] PlayIndex bug

--- src/reducers/playList.js
+++ src/reducers/playList.js
@@ -1,7 +1,15 @@
 let playList = (state = {}, action) => {
   switch(action.type) {
     case "MODIFY_PLAYLIST":
-      return Object.assign({}, state, {playList: action.playList, playIndex: action.playIndex || state.playIndex || 0, playListId: action.playListId});
+      let index;
+      if(action.playIndex === 0) {
+        index = 0;
+      } else if(!action.playIndex) {
+        index = state.playIndex || 0;
+      } else {
+        index = action.playIndex;
+      }
+      return Object.assign({}, state, {playList: action.playList, playIndex: index, playListId: action.playListId});
     case "PLAY_INDEX":
       return Object.assign({}, state, {playIndex: action.playIndex});
     default:
