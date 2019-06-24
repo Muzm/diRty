75742d84ee2c4b531d909b787f08663e5206251e
LouReed <muzhiu@gmail.com>
2019-06-24 11:15:37 +0800

    [ Feature ] Sync like list

--- src/compenets/like.js
+++ src/compenets/like.js
@@ -5,7 +5,7 @@ import "../styleSheet/login.scss";
 class Like extends React.Component {
   render() {
     return (
-      <i onClick={this.props.like} className="heart fas fa-heartbeat" title="Like Current Song"></i>
+      <i onClick={this.props.like} className={`heart fas fa-heartbeat ${this.props.liked && 'liked'}`} title="Like Current Song"></i>
     )
   }
 }
--- src/compenets/login.js
+++ src/compenets/login.js
@@ -15,40 +15,109 @@ class Login extends React.Component {
     this.state = {
       logining: true,
       phone: '',
-      password: ''
+      password: '',
+      liked: false // is liked current song
     };
   }
 
   async login() {
-    const res = await axios(`http://${apiConfig.api}/login`, {
-      method: "POST",
-      data: {
-        phone: this.state.phone,
-        password: this.state.password
-      },
-      withCredentials: true
-    });
-    
-    if(res.likeType === 1) {
-      console.log('logined');
+    try {
+      const res = await axios(`http://${apiConfig.api}/login`, {
+        method: "POST",
+        data: {
+          phone: this.state.phone,
+          password: this.state.password
+        },
+        withCredentials: true
+      });
+      
+      if(res.data.loginType === 1) {
+        console.log('res');
+        localStorage.setItem('uid', res.data.account.id);
+      }
+    } catch(e) {
+      console.log(`${e} login function: login.js 23`);
     }
   }
 
   likeSong = async () => {
-    console.log(this.props.currentSongId);
     if(this.props.currentSongId) {
-      const res = axios.get(`http://${apiConfig.search}/like?id=${this.props.currentSongId}`, {
-        withCredentials: true
-      });
-  
-      console.log(res);
+      if(!JSON.parse(localStorage.getItem('loginExpired'))) {
+        if(!this.state.liked) {
+          try {
+            const res = await axios.get(`http://${apiConfig.search}/like?id=${this.props.currentSongId}`, {
+              withCredentials: true
+            });
+            const likedSongs = JSON.parse(localStorage.getItem('likedSongs')) || {};
+            const toSave = Object.assign(likedSongs, {
+              [this.props.currentSongId]: this.props.currentSongId
+            });
+            localStorage.setItem('likedSongs', JSON.stringify(toSave));
+            console.log(res);
+          } catch(e) {
+            console.log(`${e} likeSong function: login.js 43`);
+          }
+        }
+      } else {
+        console.log('Login Expired');
+      }
     }
   };
+
+  checkLoginStatus = async () => {
+    try {
+      const res = await axios.get(`http://${apiConfig.search}/login/status`, {
+        withCredentials: true
+      });
+      localStorage.setItem('loginExpired', res.data.bindings[0]["expired"]);
+      return res.data.bindings[0]["expired"]; // true needs login false no needs login
+    } catch(e) {
+      console.log(`${e} checkLoginStatus function: login.js 67`);
+    }
+  }
+
+  async syncLikeList() {
+    const uid = localStorage.getItem('uid');
+    const isExpired = JSON.parse(localStorage.getItem('loginExpired'));
+    if(uid && !isExpired){
+      try {
+        const res = await axios.get(`http://${apiConfig.search}/likelist?uid=${uid}`, {
+            withCredentials: true
+          }
+        );
+
+        if(res.data.code === 200) {
+          let ids = res.data.ids;
+          let toSave = {};
+          ids.forEach(id => {
+            toSave = Object.assign(toSave, {[id]: id})
+          });
+          localStorage.setItem('likedSongs', JSON.stringify(toSave));
+        } else {
+          console.log("can't sync like list");
+        }
+      } catch(e) {
+        console.log(`${e} suncLikeList fucntion: login.js 79`);
+      }
+    }
+  }
 
   inputEnter = (e) => {
     if (e.keyCode === 13) {
       this.login();
     }
+  }
+
+  async componentDidMount() {
+    await this.checkLoginStatus(); // 顺序执行 先检查登陆状态再同步喜欢歌曲列表
+    await this.syncLikeList();
+  }
+  
+  componentWillReceiveProps({ currentSongId }) {
+    const isSaved = JSON.parse(
+      localStorage.getItem('likedSongs') || '{}'
+    )[currentSongId];
+    this.setState({ liked: isSaved });
   }
 
   render() {
@@ -90,7 +159,7 @@ class Login extends React.Component {
           <img alt="avatar" className="avatar" src={require('../asset/deer.jpg')}></img>
           <p className="name">{'Login to Like'}</p>
         </div>
-        <Like like={this.likeSong}></Like>
+        <Like like={this.likeSong} liked={this.state.liked}></Like>
         
       </div>
     )
